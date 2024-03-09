fe44e8859c93555325ef707c2ad62446b78b8cfa
Justin <justinnanguyen@gmail.com>
2024-03-08 22:46:13 -0600

    tab line controls

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -72,5 +72,10 @@ use({
         })
     end
 })
+--tab lines
+-- These optional plugins should be loaded directly because of a bug in Packer lazy loading
+use 'nvim-tree/nvim-web-devicons' -- OPTIONAL: for file icons
+use 'lewis6991/gitsigns.nvim' -- OPTIONAL: for git status
+use 'romgrk/barbar.nvim'
 end)
 
